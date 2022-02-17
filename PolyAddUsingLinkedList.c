adc540d9edef9a63c9cf2da7ce9b449de82e36f7
004Ajay <85185407+004Ajay@users.noreply.github.com>
2022-02-17 13:32:01 +0530

    new programs added

--- PolyAddUsingLinkedList.c
+++ PolyAddUsingLinkedList.c
@@ -1,126 +1,120 @@
-//C Program to add two polynomial using LinkedList
-//completed up to line 85
+//C Program to do Addition of polynomial using LinkedList
 
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Node
-  {
+struct Node{
    int coeff;
    int pow;
    struct Node* next;
   };
 
-void readPoly(struct Node** poly)
-  {
-   int coeff,exp,cont;
+void ReadPoly(struct Node** poly){
+   int coeff, exp, cont;
    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
-   *poly=temp;
+   *poly = temp;
 
-   do
-    {
+   do{
      printf("\nEnter Coeffecient: ");
-     scanf("%d",&coeff);
+     scanf("%d", &coeff);
+
      printf("\nEnter Exponent: ");
-     scanf("%d",&exp);
-     temp->coeff=coeff;
-     temp->pow=exp;
-     temp->next=NULL;
-     printf("\n1-Continue\n0-Next Step\n");
-     scanf("%d",&cont);
-     if(cont)
-      {
-       temp->next =(struct Node*)malloc(sizeof(struct Node));
-       temp=temp->next;
-       temp->next=NULL;
+     scanf("%d", &exp);
+
+     temp->coeff = coeff;
+     temp->pow = exp;
+     temp->next = NULL;
+
+     printf("\n1 for Continue\n0 for Next Step\n");
+     scanf("%d", &cont);
+
+     if(cont){
+
+       temp->next = (struct Node*)malloc(sizeof(struct Node));
+       temp = temp->next;
+       temp->next = NULL;
+
       }
-    }while(cont);
+    }while(cont);	
   }
 
-void displayPoly(struct Node* poly)
- {
-   printf("\nPolynomial expression is: ");
-   while(poly!=NULL)
-   {
-    printf("%dX^%d",poly->coeff,poly->pow);
-    poly=poly->next;
-    if(poly!=NULL)
-    printf("+");
+void DisplayPoly(struct Node* poly){
+   while(poly != NULL){
+    printf("%dx^%d",poly->coeff,poly->pow);
+    poly = poly->next;
+    if(poly != NULL)
+    printf(" + ");
    }
- }
+ } 
 
-void addPoly(struct Node** result,struct Node* first,struct Node* second)
-  {
-   struct Node* temp =(struct Node*)malloc(sizeof(struct Node));
-   temp->next=NULL;
-   *result=temp;
-
-   while(first && second)
-     {
-      if(first->pow>second->pow)
- 	{
- 	 temp->coeff=first->coeff;
- 	 temp->pow=first->pow;
- 	 first=first->next;
+void AddPoly(struct Node** result,struct Node* first,struct Node* second){
+   struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+   temp->next = NULL;
+   *result = temp;
+   
+   while(first && second){
+   if(first->pow>second->pow){
+ 	 temp->coeff = first->coeff;
+ 	 temp->pow = first->pow;
+ 	 first = first->next;
  	}
-      else if(first->pow<second->pow)
- 	{
- 	 temp->coeff=second->coeff;
- 	 temp->pow=second->pow;
- 	 second=second->next;
+    else if(first->pow<second->pow){
+ 	 temp->coeff = second->coeff;
+ 	 temp->pow = second->pow;
+ 	 second = second->next;
  	}
-      else
- 	{
- 	 temp->coeff=first->coeff+second->coeff;
- 	 temp->pow=first->pow;
- 	 first=first->next;
- 	 second=second->next;
+    else{
+ 	 temp->coeff = first->coeff + second->coeff;
+ 	 temp->pow = first->pow;
+ 	 first = first->next;
+ 	 second = second->next;
  	}
-      if(first && second)
- 	{
- 	 temp->next=(struct Node*)malloc(sizeof(struct Node));
- 	 temp=temp->next;
- 	 temp->next=NULL;
+    if(first && second){
+ 	 temp->next = (struct Node*)malloc(sizeof(struct Node));
+ 	 temp = temp->next;
+ 	 temp->next = NULL;
  	}
-     }
-   while(first||second)
-     {
-      temp->next=(struct Node*)malloc(sizeof(struct Node));
-      temp=temp->next;
-      temp->next=NULL;
-
-      if(first)
- 	{
- 	 temp->coeff=first->coeff;
- 	 temp->pow=first->pow;
- 	 first=first->next;
+    }
+   while(first||second){
+      temp->next = (struct Node*)malloc(sizeof(struct Node));
+      temp = temp->next;
+      temp->next = NULL;
+ 			
+    if(first){
+ 	 temp->coeff = first->coeff;
+ 	 temp->pow = first->pow;
+ 	 first = first->next;
  	}
-      else if(second)
- 	{
- 	 temp->coeff=second->coeff;
- 	 temp->pow=second->pow;
- 	 second=second->next;
+    else if(second){
+ 	 temp->coeff = second->coeff;
+ 	 temp->pow = second->pow;
+ 	 second = second->next;
  	}
-     }
+  }
   }
 
-int main()
-  {
-   struct Node*first=NULL;
-   struct Node*second=NULL;
-   struct Node*result=NULL;
-   
-   printf("\n\tPolynomial 1\n\t----------");
-   readPoly(&first);
-   
-   printf("\n\tPolynomial 2\n\t----------");
-   readPoly(&second);
-   
-   addPoly(&result, first, second);
-   
-   displayPoly(first);
-   displayPoly(second);
-   displayPoly(result);
-   
+int main(){
+
+   struct Node*first = NULL;
+   struct Node*second = NULL;
+   struct Node*result = NULL;
+
+   printf("Polynomial 1: ");
+   ReadPoly(&first);
+
+   printf("\nPolynomial 2: ");
+   ReadPoly(&second);
+
+   AddPoly(&result, first, second);
+
+   printf("\nPolynomial 1: ");
+   DisplayPoly(first);
+
+   printf("\nPolynomial 2: ");
+   DisplayPoly(second);
+
+   printf("\nResult: ");
+   DisplayPoly(result);
+
    return 0;
   }
